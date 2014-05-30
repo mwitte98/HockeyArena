be09a8639b9ce33893223481e298e43158f443f2
Matthew Witte <mcw9612@rit.edu>
2014-05-30 14:14:58 -0400

    Update environment files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+HockeyArena::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
