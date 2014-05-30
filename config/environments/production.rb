f382574080280382c3e8135a9c49bc3f9c1c8dbf
Matthew Witte <mcw9612@rit.edu>
2014-05-30 14:14:58 -0400

    Update environment files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+HockeyArena::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
