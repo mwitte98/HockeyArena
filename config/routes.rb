0301e5838c13ea14fb26150774ba04c886547659
Matthew Witte <mcw9612@rit.edu>
2014-08-18 19:56:44 -0400

    Created a detailed player page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users,        only: [:new, :create, :update, :edit]
   resources :sessions,     only: [:new, :create, :destroy]
+  resources :players,      only: [:show]
   root 'static_pages#home'
   match '/signup',      to: 'users#new',           via: 'get'
   match '/signin',      to: 'sessions#new',        via: 'get'
