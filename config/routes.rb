8ed5d6bd97c43ca17d3b62eb55a46200210b2fca
Matthew Witte <mcw9612@rit.edu>
2014-09-04 20:47:31 -0400

    Add clock to jobs

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   match '/players5556', to: 'players#show5556',    via: 'get'
   match '/players5758', to: 'players#show5758',    via: 'get'
   match '/get_info',    to: 'players#get_info',    via: 'get'
+  match '/get_U20_info', to: 'players#get_U20_info', via: 'get'
   match '/get_NT_info', to: 'players#get_NT_info', via: 'post'
   match '/login_HA',    to: 'players#login_HA',    via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
