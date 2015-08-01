ac207427e2bb2d609eed733010cf340f54a80ea6
Matthew Witte <mcw9612@rit.edu>
2015-08-01 13:38:50 -0400

    Add YS and draft into db and update script

---
+++ app/helpers/youth_school_helper.rb
@@ -0,0 +1,2 @@
+module YouthSchoolHelper
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607164257) do
+ActiveRecord::Schema.define(version: 20150725020310) do
 
-  create_table "players", force: true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
     t.integer  "playerid"
     t.string   "name"
     t.integer  "age"
@@ -37,7 +40,7 @@ ActiveRecord::Schema.define(version: 20140607164257) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
@@ -46,7 +49,22 @@ ActiveRecord::Schema.define(version: 20140607164257) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
+  create_table "youth_schools", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "quality"
+    t.string   "potential"
+    t.string   "talent"
+    t.json     "ai"
+    t.integer  "priority"
+    t.string   "manager"
+    t.string   "version"
+    t.boolean  "draft"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
