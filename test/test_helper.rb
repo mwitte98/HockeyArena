f1375258c4343d8a22645e4ffe7fd9c619efdcbb
Matthew Witte <mcw9612@rit.edu>
2016-11-19 19:04:03 -0500

    Don't store code coverage in circleci artifacts

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,12 +7,6 @@ require 'simplecov'
 # Minitest output with color and progress bar
 Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new, ENV, Minitest.backtrace_filter)
 
-# Save to CircleCI's artifacts directory if we're on CircleCI
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  SimpleCov.coverage_dir(dir)
-end
-
 SimpleCov.start 'rails' do
   add_filter '.env'
 end
