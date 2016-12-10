8374f488398c19251ca62179c9dce57e010abe0f
Matthew Witte <mcw9612@rit.edu>
2016-12-10 16:54:45 -0500

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,12 @@ Rails.application.routes.draw do
     end
   end
   root 'static_pages#home'
-  match '/signup',          to: 'users#new',           via: 'get'
-  match '/signin',          to: 'sessions#new',        via: 'get'
-  match '/signout',         to: 'sessions#destroy',    via: 'delete'
-  match '/players6566',     to: 'players#show6566',    via: 'get'
-  match '/players6768',     to: 'players#show6768',    via: 'get'
-  match '/playersSenior',   to: 'players#show_senior', via: 'get'
-  match '/update_info',     to: 'players#update_info', via: 'get'
-  match '/delete_all',      to: 'players#delete_all',  via: 'post', as: 'delete_all'
-  match '/speedoBetaDraft', to: 'youth_school#speedo_beta_draft', via: 'get'
-  match '/speedoBetaYS',    to: 'youth_school#speedo_beta_ys',    via: 'get'
-  match '/speedoLiveDraft', to: 'youth_school#speedo_live_draft', via: 'get'
-  match '/speedoLiveYS',    to: 'youth_school#speedo_live_ys',    via: 'get'
-  match '/speedyBetaDraft', to: 'youth_school#speedy_beta_draft', via: 'get'
-  match '/speedyBetaYS',    to: 'youth_school#speedy_beta_ys',    via: 'get'
-  match '/speedyLiveDraft', to: 'youth_school#speedy_live_draft', via: 'get'
-  match '/speedyLiveYS',    to: 'youth_school#speedy_live_ys',    via: 'get'
+  match '/signup', to: 'users#new', via: :get
+  match '/signin', to: 'sessions#new', via: :get
+  match '/signout', to: 'sessions#destroy', via: :delete
+  match '/national/:team', to: 'players#show_national_team', via: :get, as: 'national'
+  match '/update_info', to: 'players#update_info', via: :get
+  match '/delete_all', to: 'players#delete_all', via: :post, as: 'delete_all'
+  match '/:manager/:version/:type', to: 'youth_school#show', via: :get, as: 'ys'
+  match '*path', to: 'static_pages#home', via: :get
 end
