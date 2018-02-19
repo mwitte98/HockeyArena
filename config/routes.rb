a9ed00249d3b746c02a3893861d9036633e3fe86
Matthew Witte <mcw9612@rit.edu>
2018-02-18 18:38:53 -0600

    Update versions (#28)
    * Update versions
    
    * Change code climate test reporter source
    
    * Export token before reporting test coverage
    
    * Split export token and report results into 2 lines
    
    * Pass token as a param to report coverage command

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users,        only: [:new,  :create, :update, :edit]
-  resources :sessions,     only: [:new,  :create, :destroy]
+  resources :users,        only: %i[new create update edit]
+  resources :sessions,     only: %i[new create destroy]
   resources :players,      only: [:show]
   resources :players do
     collection do
