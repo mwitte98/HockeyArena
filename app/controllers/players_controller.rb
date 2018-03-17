a354baf78d9f83c7024e0f2620fbb79129346bc7
Matthew Witte <mcw9612@rit.edu>
2018-03-17 15:48:47 -0500

    Improve quality of controllers and helpers (#36)
    * Improve quality of calculate_ai method in PlayersHelper
    
    * Improve quality of show_national_team and delete_all methods in PlayersController
    
    * Improve quality of YouthSchoolController
    
    * Clean up class variable usage in YouthSchoolController

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,25 +2,10 @@ class PlayersController < ApplicationController
   before_action :signed_in_user
 
   def show_national_team
-    params_team = params[:team]
-    team = if params_team == 'u20_active'
-             ENV['U20_20_seasons']
-           elsif params_team == 'u20_next'
-             ENV['U20_18_seasons']
-           else
-             'senior'
-           end
     @players = []
-    @player_instances = Player.where(team: team)
+    @player_instances = Player.where(team: params[:team])
     @player_instances.each do |instance|
-      daily = instance.daily
-      keys = []
-      daily.keys.sort.each { |key| keys << key }
-      dates = keys[-2..-1]
-      dates = keys if dates.nil?
-      player = []
-      dates.each { |date| player << daily[date] }
-      @players << player
+      @players << get_last_two(instance.daily)
     end
   end
 
@@ -35,22 +20,14 @@ class PlayersController < ApplicationController
 
   def delete_all
     player = Player.find(params[:id])
-    player_name = player.name
-    player_team = player.team
-    Player.delete_all(['name = ?', player_name])
-    flash[:success] = "#{player_name} deleted."
-    if player_team == ENV['U20_20_seasons']
-      redirect_to national_path(team: 'u20_active')
-    elsif player_team == ENV['U20_18_seasons']
-      redirect_to national_path(team: 'u20_next')
-    else
-      redirect_to national_path(team: 'senior')
-    end
+    player.destroy
+    flash[:success] = "#{player.name} deleted."
+    redirect_to national_path(team: player.team)
   end
 
   def update_info
     UpdateJob.perform_async
-    redirect_to national_path(team: 'u20_active')
+    redirect_to national_path(team: ENV['U20_20_seasons'])
   end
 
   private
@@ -59,5 +36,15 @@ class PlayersController < ApplicationController
     return if signed_in?
     flash[:warning] = 'You must be signed in to access that page.'
     redirect_to root_url
+  end
+
+  def get_last_two(daily)
+    keys = []
+    daily.keys.sort.each { |key| keys << key }
+    dates = keys[-2..-1]
+    dates = keys if dates.nil?
+    player = []
+    dates.each { |date| player << daily[date] }
+    player
   end
 end
