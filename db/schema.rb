a3a319da06017380462457371972622631bb4445
Matthew Witte <mcw9612@rit.edu>
2020-03-15 00:56:31 -0500

    Use playerid instead of name for ys/draft script (#49)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_193012) do
+ActiveRecord::Schema.define(version: 2020_03_15_014833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_11_10_193012) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "team"
+    t.string "playerid"
   end
 
 end
